15434e7b28df4eef38a980ef41b26fef25cdc1a6
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-24 09:04:00 -0700

    fixed small error

--- sum-of-odd-cubes/solution.js
+++ sum-of-odd-cubes/solution.js
@@ -1,8 +1,8 @@
 function cubeOdd(arr) {
   let newArr = arr.map(x => Math.pow(x, 3));
   let newerArray = newArr.filter(x => x % 2 !== 0);
-  if(newerArray === []){
-    return undefined;}
+  if(newerArray.length < 1){
+    return 0;}
     else{
     let newestArr = newerArray.reduce((accum, count) => accum += count);
     if(newestArr || newestArr === 0){
